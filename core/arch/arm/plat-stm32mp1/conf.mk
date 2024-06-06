666dc48d3ffc8097e52ba20e2615065e3dda4607
Etienne Carriere <etienne.carriere@foss.st.com>
2024-06-06 09:24:26 +0200

    plat-stm32mp1: enable SCMI regulator consumer for STM32MP15
    Default enable support for SCMI regulator consumer on STM32MP15 platform.
    This change requires a rework of the SCMI protocol list passed to
    SCMI client as SCMI voltage Domain protocol should not be listed if
    there are no voltage domains exposed (aka no scmi-regulator-consumer
    compatible DT node) despite CFG_SCMI_MSG_REGULATOR_CONSUMER is enabled.
    
    Change-Id: I72137f3092668b8a7179429125bc0b94f2cc7994
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/385768
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -406,10 +406,8 @@ $(call force,CFG_SCMI_MSG_DRIVERS,y)
 $(call force,CFG_SCMI_SCPFW,n,SCPFW for STM32MP1 no is not supported)
 $(call force,CFG_SCMI_MSG_CLOCK,y)
 $(call force,CFG_SCMI_MSG_RESET_DOMAIN,y)
-ifeq ($(CFG_STM32MP13),y)
 $(call force,CFG_SCMI_MSG_REGULATOR_CONSUMER,y)
 $(call force,CFG_SCMI_MSG_VOLTAGE_DOMAIN,y)
-endif
 CFG_SCMI_MSG_SHM_MSG ?= y
 CFG_SCMI_MSG_SMT ?= y
 CFG_SCMI_MSG_SMT_THREAD_ENTRY ?= y
