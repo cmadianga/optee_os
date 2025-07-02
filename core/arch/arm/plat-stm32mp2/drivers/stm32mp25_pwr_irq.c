gradhocJcalvo/optee-graddeck

commit ad7b90375efee694381e86a5e3dac4ec604cc6cf

plat-stm32mp2: make PWR IRQ independent from EXTI
The PWR IRQ is an interrupt controller that is hierarchical placed
between EXTI and GIC.
While still handle the current code that calls EXTI API, let the
driver PWR IRQ to work standalone relying to the hierarchical
calls.

Signed-off-by: Antonio Borneo <antonio.borneo@foss.st.com>
Change-Id: I9fb58246345e8c3cdfd4bf997a4109a75427833f
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/434937
Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/arch/arm/plat-stm32mp2/drivers/stm32mp25_pwr_irq.c
@@ -145,7 +145,7 @@ static void stm32mp25_pwr_itr_enable_nolock(size_t it)
 
 	VERBOSE_PWR("Pwr irq enable %zu", it);
 
-	if (IS_ENABLED(CFG_STM32_EXTI)) {
+	if (IS_ENABLED(CFG_STM32_EXTI) && priv->exti) {
 		stm32_exti_unmask(priv->exti, PWR_EXTI_WKUP1 + it);
 		stm32_exti_enable_wake(priv->exti, PWR_EXTI_WKUP1 + it);
 	}
@@ -161,7 +161,7 @@ static void stm32mp25_pwr_itr_disable_nolock(size_t it)
 
 	io_clrbits32(pwr_wkupcr_base(it), WKUPENCPU1);
 
-	if (IS_ENABLED(CFG_STM32_EXTI)) {
+	if (IS_ENABLED(CFG_STM32_EXTI) && priv->exti) {
 		stm32_exti_mask(priv->exti, PWR_EXTI_WKUP1 + it);
 		stm32_exti_disable_wake(priv->exti, PWR_EXTI_WKUP1 + it);
 	}
@@ -403,7 +403,7 @@ static TEE_Result stm32mp2_pwr_itr_dt_get(struct dt_pargs *args,
 		return res;
 	}
 
-	if (IS_ENABLED(CFG_STM32_EXTI))
+	if (IS_ENABLED(CFG_STM32_EXTI) && priv->exti)
 		stm32_exti_set_tz(priv->exti, PWR_EXTI_WKUP1 + itr_num);
 
 	itr_desc_p->chip = &stm32mp2_pwr_itr_chip;
@@ -435,7 +435,7 @@ TEE_Result stm32mp25_pwr_irq_probe(const void *fdt, int node)
 	if (IS_ENABLED(CFG_STM32_EXTI)) {
 		res = stm32_exti_get_pdata(fdt, node, &priv->exti);
 		if (res)
-			goto err;
+			priv->exti = NULL;
 	}
 
 	res = interrupt_dt_get(fdt, node, &itr_chip, &itr_num);
@@ -458,12 +458,6 @@ TEE_Result stm32mp25_pwr_irq_probe(const void *fdt, int node)
 	VERBOSE_PWR("Init pwr irq done");
 
 	return TEE_SUCCESS;
-
-err:
-	free(pwr_wkup_d);
-	pwr_wkup_d = NULL;
-
-	return res;
 }
 
 static enum itr_return pwr_it_user_handler(struct itr_handler *handler)

cmadianga/optee_os

commit 17f5addba6a977e75732e997f5d24952de169173

plat-stm32mp2: remove exti in struct pwr_wkup_data
Remove the unused field pwr_wkup_data::exti

Fixes: ad7b90375efe ("plat-stm32mp2: make PWR IRQ independent from EXTI")
Signed-off-by: Antonio Borneo <antonio.borneo@foss.st.com>
Change-Id: Iaddcd9ef9bf9a01d24bde5671e5ed4e4cdbe31e8
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/462461
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
Domain-Review: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

core/arch/arm/plat-stm32mp2/drivers/stm32mp25_pwr_irq.c
@@ -51,7 +51,6 @@ enum wkup_pull_setting {
 struct pwr_wkup_data {
 	struct itr_chip *pwr_irq_chip;
 	struct itr_handler *parent_hdl;
-	struct stm32_exti_pdata *exti;
 	unsigned int spinlock;
 	uint8_t itr_enable_bitmask;
 	uint8_t itr_mask_bitmask;