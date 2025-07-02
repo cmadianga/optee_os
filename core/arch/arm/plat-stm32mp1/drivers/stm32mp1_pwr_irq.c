gradhocJcalvo/optee-graddeck

commit b5ff74d2114bc4820f5d116af3adedef72aa6053

plat-stm32mp1: make PWR IRQ independent from EXTI
The PWR IRQ is an interrupt controller that is hierarchical placed
between EXTI and GIC.
While still handle the current code that calls EXTI API, let the
driver PWR IRQ to work standalone relying to the hierarchical
calls.

Signed-off-by: Antonio Borneo <antonio.borneo@foss.st.com>
Change-Id: I659a44a63d81d5521a0b0db45c1c57d679715959
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/434936
Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pwr_irq.c
@@ -154,7 +154,7 @@ static void stm32mp1_pwr_itr_enable_nolock(size_t it)
 
 	VERBOSE_PWR("Pwr irq enable");
 
-	if (IS_ENABLED(CFG_STM32_EXTI)) {
+	if (IS_ENABLED(CFG_STM32_EXTI) && priv->exti) {
 		stm32_exti_unmask(priv->exti, PWR_EXTI_WKUP1 + it);
 		stm32_exti_enable_wake(priv->exti, PWR_EXTI_WKUP1 + it);
 	}
@@ -170,7 +170,7 @@ static void stm32mp1_pwr_itr_disable_nolock(size_t it)
 
 	io_clrbits32(priv->base + MPUWKUPENR, BIT(it));
 
-	if (IS_ENABLED(CFG_STM32_EXTI)) {
+	if (IS_ENABLED(CFG_STM32_EXTI) && priv->exti) {
 		stm32_exti_mask(priv->exti, PWR_EXTI_WKUP1 + it);
 		stm32_exti_disable_wake(priv->exti, PWR_EXTI_WKUP1 + it);
 	}
@@ -412,7 +412,7 @@ static TEE_Result stm32mp1_pwr_itr_dt_get(struct dt_pargs *args,
 		return res;
 	}
 
-	if (IS_ENABLED(CFG_STM32_EXTI))
+	if (IS_ENABLED(CFG_STM32_EXTI) && priv->exti)
 		stm32_exti_set_tz(priv->exti, PWR_EXTI_WKUP1 + itr_num);
 
 	itr_desc_p->chip = &stm32mp1_pwr_itr_chip;
@@ -446,7 +446,7 @@ static TEE_Result stm32mp1_pwr_irq_probe(const void *fdt, int node,
 	if (IS_ENABLED(CFG_STM32_EXTI)) {
 		res = stm32_exti_get_pdata(fdt, node, &priv->exti);
 		if (res)
-			goto err;
+			priv->exti = NULL;
 	}
 
 	res = interrupt_dt_get(fdt, node, &itr_chip, &itr_num);

cmadianga/optee_os

commit c0d13e0eaead1cbe60b9213ab6976b8f015270e3

plat-stm32mp1: remove exti in struct pwr_wkup_data
Remove the unused field stm32_pwr_data::exti

Fixes: b5ff74d2114b ("plat-stm32mp1: make PWR IRQ independent from EXTI")
Signed-off-by: Antonio Borneo <antonio.borneo@foss.st.com>
Change-Id: Ia01e2cc07a4b2c89786be8c7743024efd9e63aed
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/462460
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Domain-Review: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pwr_irq.c
@@ -48,7 +48,6 @@ struct stm32_pwr_data {
 	vaddr_t base;
 	struct itr_chip *pwr_irq_chip;
 	struct itr_handler *parent_hdl;
-	struct stm32_exti_pdata *exti;
 	unsigned int spinlock;
 	uint8_t itr_enable_bitmask;
 	uint8_t itr_mask_bitmask;