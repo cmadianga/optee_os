gradhocJcalvo/optee-graddeck

commit e69b46dbd78e5a74df78ff0d224553edc1385cfd

drivers: stm32mp2_pmic: handle STPMIC1L and STPMIC2L
Handle STPMCI1L and STPMIC2L versions.
- Buck1 can have an extended voltage range that is configured in
NVM at boot time.
- Those pmic can drive external regulators thank's to their GPO.

Change-Id: I9fe9906d49c05cea9f0d2c1529d54ae793806c02
Signed-off-by: Pascal Paillet <p.paillet@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/378250
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>

core/drivers/stpmic2.c
@@ -38,7 +38,7 @@ struct regul_struct {
 #define LDO_SINGLE_VOLT_TABLE_SIZE 1
 
 /* Voltage tables in mV */
-static const uint16_t buck1236_volt_table[BUCK_VOLT_TABLE_SIZE] = {
+static const uint16_t buck_low_volt_table[BUCK_VOLT_TABLE_SIZE] = {
 	U(500), U(510), U(520), U(530), U(540),
 	U(550), U(560), U(570), U(580), U(590),
 	U(600), U(610), U(620), U(630), U(640),
@@ -67,7 +67,7 @@ static const uint16_t buck1236_volt_table[BUCK_VOLT_TABLE_SIZE] = {
 	U(1500), U(1500), U(1500)
 };
 
-static const uint16_t buck457_volt_table[BUCK_VOLT_TABLE_SIZE] = {
+static const uint16_t buck_high_volt_table[BUCK_VOLT_TABLE_SIZE] = {
 	U(1500), U(1500), U(1500), U(1500), U(1500),
 	U(1500), U(1500), U(1500), U(1500), U(1500),
 	U(1500), U(1500), U(1500), U(1500), U(1500),
@@ -173,22 +173,33 @@ static const uint16_t refddr_volt_table[LDO_SINGLE_VOLT_TABLE_SIZE] = {
 	.ocp_mask		= FS_OCP_ ## ID, \
 }
 
+#define DEFINE_GPOx(regu_name, ID) { \
+	.name			= regu_name, \
+	.en_cr			= ID ## _MAIN_CR, \
+	.alt_en_cr		= ID ## _ALT_CR, \
+	.pwrctrl_cr		= ID ## _PWRCTRL_CR, \
+	.msrt_reg		= GPO_MRST_CR, \
+	.msrt_mask		= ID ## _MRST, \
+}
+
 /* Table of Regulators in PMIC SoC */
 static const struct regul_struct regul_table[] = {
 	[STPMIC2_BUCK1] = DEFINE_BUCK("buck1", BUCK1, BUCKS_PD_CR1,
-				      buck1236_volt_table),
+				      buck_low_volt_table),
+	[STPMIC2_BUCK1H] = DEFINE_BUCK("buck1h", BUCK1, BUCKS_PD_CR1,
+				      buck_high_volt_table),
 	[STPMIC2_BUCK2] = DEFINE_BUCK("buck2", BUCK2, BUCKS_PD_CR1,
-				      buck1236_volt_table),
+				      buck_low_volt_table),
 	[STPMIC2_BUCK3] = DEFINE_BUCK("buck3", BUCK3, BUCKS_PD_CR1,
-				      buck1236_volt_table),
+				      buck_low_volt_table),
 	[STPMIC2_BUCK4] = DEFINE_BUCK("buck4", BUCK4, BUCKS_PD_CR1,
-				      buck457_volt_table),
+				      buck_high_volt_table),
 	[STPMIC2_BUCK5] = DEFINE_BUCK("buck5", BUCK5, BUCKS_PD_CR2,
-				      buck457_volt_table),
+				      buck_high_volt_table),
 	[STPMIC2_BUCK6] = DEFINE_BUCK("buck6", BUCK6, BUCKS_PD_CR2,
-				      buck1236_volt_table),
+				      buck_low_volt_table),
 	[STPMIC2_BUCK7] = DEFINE_BUCK("buck7", BUCK7, BUCKS_PD_CR2,
-				      buck457_volt_table),
+				      buck_high_volt_table),
 
 	[STPMIC2_REFDDR] = DEFINE_REFDDR("refddr", REFDDR, refddr_volt_table),
 
@@ -209,7 +220,27 @@ static const struct regul_struct regul_table[] = {
 	[STPMIC2_LDO8] = DEFINE_LDOx("ldo8", LDO8, ldo235678_volt_table,
 				     LDO235678_VOLT_TABLE_SIZE),
 
+	[STPMIC2_GPO1] = DEFINE_GPOx("gpo1", GPO1),
+	[STPMIC2_GPO2] = DEFINE_GPOx("gpo2", GPO2),
+	[STPMIC2_GPO3] = DEFINE_GPOx("gpo3", GPO3),
+	[STPMIC2_GPO4] = DEFINE_GPOx("gpo4", GPO4),
+	[STPMIC2_GPO5] = DEFINE_GPOx("gpo5", GPO5),
 };
+
+TEE_Result stpmic2_regulator_get_id(const char *regu_name, uint8_t *id)
+{
+	uint8_t i = 0;
+
+	FMSG("Stpmic2 get id %s", regu_name);
+
+	for (i = 0; i < ARRAY_SIZE(regul_table); i++)
+		if (!strcmp(regul_table[i].name, regu_name)) {
+			*id = i;
+			return TEE_SUCCESS;
+		}
+
+	return TEE_ERROR_ITEM_NOT_FOUND;
+}
 
 TEE_Result stpmic2_register_read(struct stpmic2 *pmic,
 				 uint8_t register_id, uint8_t *value)
@@ -401,7 +432,8 @@ TEE_Result stpmic2_regulator_get_prop(struct stpmic2 *pmic, uint8_t id,
 		*arg = PROP_BYPASS_RESET;
 
 		if (id <= STPMIC2_BUCK7 || id == STPMIC2_LDO1 ||
-		    id == STPMIC2_LDO4 || id == STPMIC2_REFDDR)
+		    id == STPMIC2_LDO4 || id == STPMIC2_REFDDR ||
+		    pmic->ref_id != PMIC_REF_ID_STPMIC25)
 			return TEE_SUCCESS;
 
 		res = stpmic2_register_read(pmic, regul->en_cr, &val);
@@ -442,7 +474,8 @@ TEE_Result stpmic2_regulator_set_prop(struct stpmic2 *pmic, uint8_t id,
 					       regul->msrt_mask);
 	case STPMIC2_BYPASS:
 		if (id <= STPMIC2_BUCK7 || id == STPMIC2_LDO1 ||
-		    id == STPMIC2_LDO4 || id == STPMIC2_REFDDR)
+		    id == STPMIC2_LDO4 || id == STPMIC2_REFDDR ||
+		    pmic->ref_id != PMIC_REF_ID_STPMIC25)
 			return TEE_ERROR_BAD_PARAMETERS;
 
 		/* clear sink source mode */
@@ -468,6 +501,8 @@ TEE_Result stpmic2_regulator_set_prop(struct stpmic2 *pmic, uint8_t id,
 		return stpmic2_update_en_crs(pmic, id, LDO3_SNK_SRC,
 					     LDO3_SNK_SRC);
 	case STPMIC2_OCP:
+		if (!regul->ocp_reg)
+			return TEE_ERROR_BAD_PARAMETERS;
 		/* enable mask reset */
 		return stpmic2_register_update(pmic, regul->ocp_reg,
 					       U(0), regul->ocp_mask);
@@ -561,6 +596,23 @@ TEE_Result stpmic2_get_version(struct stpmic2 *pmic, uint8_t *val)
 TEE_Result stpmic2_get_product_id(struct stpmic2 *pmic, uint8_t *val)
 {
 	return stpmic2_register_read(pmic, PRODUCT_ID, val);
+}
+
+TEE_Result stpmic2_is_buck1_high_voltage(struct stpmic2 *pmic, bool *high)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	uint8_t val = 0;
+
+	if (pmic->ref_id == PMIC_REF_ID_STPMIC25) {
+		*high = false;
+		return TEE_SUCCESS;
+	}
+
+	res = stpmic2_register_read(pmic,  NVM_BUCK1_VOUT_SHR, &val);
+
+	*high = val & BUCK1_VRAN_GE_CFG;
+
+	return res;
 }
 
 /*

core/include/drivers/stpmic2.h
@@ -13,6 +13,7 @@
 
 enum {
 	STPMIC2_BUCK1 = 0,
+	STPMIC2_BUCK1H,
 	STPMIC2_BUCK2,
 	STPMIC2_BUCK3,
 	STPMIC2_BUCK4,
@@ -28,6 +29,11 @@ enum {
 	STPMIC2_LDO6,
 	STPMIC2_LDO7,
 	STPMIC2_LDO8,
+	STPMIC2_GPO1,
+	STPMIC2_GPO2,
+	STPMIC2_GPO3,
+	STPMIC2_GPO4,
+	STPMIC2_GPO5,
 	STPMIC2_NB_REG,
 };
 
@@ -59,6 +65,7 @@ enum {
 #define BUCKS_PD_CR2		U(0x1A)
 #define LDOS_PD_CR1		U(0x1B)
 #define LDOS_PD_CR2		U(0x1C)
+#define GPO_MRST_CR		U(0x1C)
 #define BUCKS_MRST_CR		U(0x1D)
 #define LDOS_MRST_CR		U(0x1E)
 /* Buck CR */
@@ -97,6 +104,13 @@ enum {
 #define BUCK7_ALT_CR1		U(0x40)
 #define BUCK7_ALT_CR2		U(0x41)
 #define BUCK7_PWRCTRL_CR	U(0x42)
+/* GPO CR used only on PMIC1L and PMIC2L*/
+#define GPO1_MAIN_CR		U(0x43)
+#define GPO1_ALT_CR		U(0x44)
+#define GPO1_PWRCTRL_CR		U(0x45)
+#define GPO2_MAIN_CR		U(0x46)
+#define GPO2_ALT_CR		U(0x47)
+#define GPO2_PWRCTRL_CR		U(0x48)
 /* LDO CR */
 #define LDO1_MAIN_CR		U(0x4C)
 #define LDO1_ALT_CR		U(0x4D)
@@ -125,6 +139,16 @@ enum {
 #define REFDDR_MAIN_CR		U(0x64)
 #define REFDDR_ALT_CR		U(0x65)
 #define REFDDR_PWRCTRL_CR	U(0x66)
+/* GPO CR used only on PMIC1L and PMIC2L*/
+#define GPO3_MAIN_CR		U(0x67)
+#define GPO3_ALT_CR		U(0x68)
+#define GPO3_PWRCTRL_CR		U(0x69)
+#define GPO4_MAIN_CR		U(0x6A)
+#define GPO4_ALT_CR		U(0x6B)
+#define GPO4_PWRCTRL_CR		U(0x6C)
+#define GPO5_MAIN_CR		U(0x6D)
+#define GPO5_ALT_CR		U(0x6E)
+#define GPO5_PWRCTRL_CR		U(0x6F)
 /* INTERRUPT CR */
 #define INT_PENDING_R1		U(0x70)
 #define INT_PENDING_R2		U(0x71)
@@ -147,8 +171,16 @@ enum {
 #define INT_DBG_LATCH_R3	U(0x82)
 #define INT_DBG_LATCH_R4	U(0x83)
 
+/* NVMEM shadow registers */
+#define NVM_BUCK1_VOUT_SHR	U(0x9C)
+
 /* PRODUCT_ID bits definition */
 #define PMIC_NVM_ID_MASK	GENMASK_32(3, 0)
+#define PMIC_REF_ID_MASK	GENMASK_32(7, 4)
+#define PMIC_REF_ID_SHIFT	4
+#define PMIC_REF_ID_STPMIC1L	U(1)
+#define PMIC_REF_ID_STPMIC25	U(2)
+#define PMIC_REF_ID_STPMIC2L	U(3)
 
 /* VERSION_SR bits definition */
 #define MINOR_VERSION_MASK	GENMASK_32(3, 0)
@@ -174,6 +206,13 @@ enum {
 #define LDO6_MRST		BIT(5)
 #define LDO7_MRST		BIT(6)
 #define LDO8_MRST		BIT(7)
+
+/* GPO_MRST_CR bits definition */
+#define GPO1_MRST		BIT(1)
+#define GPO2_MRST		BIT(2)
+#define GPO3_MRST		BIT(3)
+#define GPO4_MRST		BIT(4)
+#define GPO5_MRST		BIT(5)
 
 /* LDOx_MAIN_CR */
 #define LDO_VOLT_SHIFT		1
@@ -246,6 +285,9 @@ enum {
 #define FS_OCP_LDO7		BIT(6)
 #define FS_OCP_LDO8		BIT(7)
 
+/* NVM_BUCK1_VOUT_SHR */
+#define BUCK1_VRAN_GE_CFG	BIT(7)
+
 enum stpmic2_prop_id {
 	STPMIC2_MASK_RESET = 0,
 	STPMIC2_PULL_DOWN,
@@ -277,12 +319,16 @@ struct pmic_it_handle_s {
  */
 struct stpmic2 {
 	struct i2c_handle_s *pmic_i2c_handle;
+	const void *compat_data;
 	uint16_t pmic_i2c_addr;
 	uint16_t irq_count;
 #ifdef CFG_STM32_PWR_IRQ
 	SLIST_HEAD(, pmic_it_handle_s) it_list;
 #endif
+	uint8_t ref_id;
 };
+
+TEE_Result stpmic2_regulator_get_id(const char *regu_name, uint8_t *id);
 
 TEE_Result stpmic2_register_read(struct stpmic2 *pmic, uint8_t register_id,
 				 uint8_t *value);
@@ -312,6 +358,7 @@ TEE_Result stpmic2_regulator_set_prop(struct stpmic2 *pmic, uint8_t id,
 void stpmic2_dump_regulators(struct stpmic2 *pmic);
 TEE_Result stpmic2_get_version(struct stpmic2 *pmic, uint8_t *val);
 TEE_Result stpmic2_get_product_id(struct stpmic2 *pmic, uint8_t *val);
+TEE_Result stpmic2_is_buck1_high_voltage(struct stpmic2 *pmic, bool *high);
 
 /* Low Power handling */
 TEE_Result stpmic2_lp_set_state(struct stpmic2 *pmic, uint8_t id, bool enable);

core/arch/arm/plat-stm32mp2/drivers/stm32mp2_pmic.c
@@ -64,6 +64,18 @@ static bool stm32_pmic2;
 #define STPMIC2_LP_STATE_SET_VOLT	BIT(3)
 
 /*
+ * struct pmic_compat_data - pmic specificity helper
+ * @desc_table: description table of each regulator
+ * @desc_len: len of the description table
+ * @ref_id: value of PMIC_REF_ID in PRODUCT_ID_SR
+ */
+struct pmic_compat_data {
+	const struct regu_dt_desc *desc_table;
+	size_t desc_len;
+	uint8_t ref_id;
+};
+
+/*
  * struct pmic_regu - STPMIC2 regulator instance
  *
  * @pmic: Handle to PMIC device
@@ -398,7 +410,8 @@ static TEE_Result apply_pm_state(struct regulator *regulator, uint8_t mode)
 		if (res)
 			return res;
 
-		if (state & STPMIC2_LP_STATE_SET_VOLT) {
+		if (state & STPMIC2_LP_STATE_SET_VOLT &&
+		    regulator->ops->set_voltage)  {
 			res = stpmic2_lp_set_voltage(regu->pmic, regu->id,
 						     lp_level_uv / 1000);
 			if (res)
@@ -512,39 +525,82 @@ static const struct regulator_ops pmic_regu_ops = {
 	.supplied_init = pmic_supplied_init,
 };
 
+static const struct regulator_ops pmic_switch_ops = {
+	.set_state = pmic_set_state,
+	.get_state = pmic_get_state,
+	.init = pmic_init,
+	.supplied_init = pmic_supplied_init,
+};
+
 #define DEFINE_REGU(_name) { \
 		.name = (_name), \
 		.ops = &pmic_regu_ops, \
 	}
 
-static const struct regu_dt_desc pmic_reguls[STPMIC2_NB_REG] = {
-	[STPMIC2_BUCK1] = DEFINE_REGU("buck1"),
-	[STPMIC2_BUCK2] = DEFINE_REGU("buck2"),
-	[STPMIC2_BUCK3] = DEFINE_REGU("buck3"),
-	[STPMIC2_BUCK4] = DEFINE_REGU("buck4"),
-	[STPMIC2_BUCK5] = DEFINE_REGU("buck5"),
-	[STPMIC2_BUCK6] = DEFINE_REGU("buck6"),
-	[STPMIC2_BUCK7] = DEFINE_REGU("buck7"),
+#define DEFINE_SWITCH(_name) { \
+		.name = (_name), \
+		.ops = &pmic_switch_ops, \
+	}
 
-	[STPMIC2_LDO1] = DEFINE_REGU("ldo1"),
-	[STPMIC2_LDO2] = DEFINE_REGU("ldo2"),
-	[STPMIC2_LDO3] = DEFINE_REGU("ldo3"),
-	[STPMIC2_LDO4] = DEFINE_REGU("ldo4"),
-	[STPMIC2_LDO5] = DEFINE_REGU("ldo5"),
-	[STPMIC2_LDO6] = DEFINE_REGU("ldo6"),
-	[STPMIC2_LDO7] = DEFINE_REGU("ldo7"),
-	[STPMIC2_LDO8] = DEFINE_REGU("ldo8"),
-
-	[STPMIC2_REFDDR] = DEFINE_REGU("refddr"),
+static const struct regu_dt_desc pmic25_reguls[] = {
+	DEFINE_REGU("buck1"),
+	DEFINE_REGU("buck2"),
+	DEFINE_REGU("buck3"),
+	DEFINE_REGU("buck4"),
+	DEFINE_REGU("buck5"),
+	DEFINE_REGU("buck6"),
+	DEFINE_REGU("buck7"),
+	DEFINE_REGU("ldo1"),
+	DEFINE_REGU("ldo2"),
+	DEFINE_REGU("ldo3"),
+	DEFINE_REGU("ldo4"),
+	DEFINE_REGU("ldo5"),
+	DEFINE_REGU("ldo6"),
+	DEFINE_REGU("ldo7"),
+	DEFINE_REGU("ldo8"),
+	DEFINE_SWITCH("refddr"),
 };
-DECLARE_KEEP_PAGER(pmic_reguls);
+DECLARE_KEEP_PAGER(pmic25_reguls);
+
+static const struct regu_dt_desc pmic1l_reguls[] = {
+	DEFINE_REGU("buck1"),
+	DEFINE_REGU("buck1h"),
+	DEFINE_REGU("buck2"),
+	DEFINE_REGU("ldo2"),
+	DEFINE_REGU("ldo3"),
+	DEFINE_REGU("ldo4"),
+	DEFINE_REGU("ldo5"),
+	DEFINE_SWITCH("gpo1"),
+	DEFINE_SWITCH("gpo2"),
+};
+DECLARE_KEEP_PAGER(pmic1l_reguls);
+
+static const struct regu_dt_desc pmic2l_reguls[] = {
+	DEFINE_REGU("buck1"),
+	DEFINE_REGU("buck1h"),
+	DEFINE_REGU("buck2"),
+	DEFINE_REGU("buck3"),
+	DEFINE_REGU("ldo1"),
+	DEFINE_REGU("ldo2"),
+	DEFINE_REGU("ldo3"),
+	DEFINE_REGU("ldo4"),
+	DEFINE_REGU("ldo5"),
+	DEFINE_REGU("ldo6"),
+	DEFINE_REGU("ldo7"),
+	DEFINE_SWITCH("gpo1"),
+	DEFINE_SWITCH("gpo2"),
+	DEFINE_SWITCH("gpo3"),
+	DEFINE_SWITCH("gpo4"),
+	DEFINE_SWITCH("gpo5"),
+};
+DECLARE_KEEP_PAGER(pmic2l_reguls);
 
 static void pmic_parse_regu_node(struct regu_dt_desc *regu_desc,
 				 const void *fdt, int node)
 {
 	struct pmic_regu *regu = regu_desc->priv;
 	const fdt32_t *cuint = NULL;
-	const char __maybe_unused *regu_name = pmic_reguls[regu->id].name;
+	const char __maybe_unused *regu_name = regu_desc->name;
 
 	cuint = fdt_getprop(fdt, node, "st,regulator-bypass-microvolt", NULL);
 	if (!cuint)
@@ -555,10 +611,11 @@ static void pmic_parse_regu_node(struct regu_dt_desc *regu_desc,
 }
 
 static void parse_low_power_mode(const void *fdt, int node,
-				 struct pmic_regu *regu, int mode)
+				 struct regu_dt_desc *regu_desc, int mode)
 {
+	struct pmic_regu *regu = regu_desc->priv;
 	const fdt32_t *cuint = NULL;
-	const char __maybe_unused *regu_name = pmic_reguls[regu->id].name;
+	const char __maybe_unused *regu_name = regu_desc->name;
 
 	regu->lp_state[mode] = 0;
 
@@ -584,7 +641,7 @@ static void parse_low_power_mode(const void *fdt, int node,
 }
 
 static void parse_low_power_modes(const void *fdt, int node,
-				  struct pmic_regu *regu)
+				  struct regu_dt_desc *regu_desc)
 {
 	const size_t lp_mode_count = plat_get_lp_mode_count();
 	unsigned int mode = 0;
@@ -598,7 +655,7 @@ static void parse_low_power_modes(const void *fdt, int node,
 			/* Get the configs from regulator_state_node subnode */
 			n = fdt_subnode_offset(fdt, node, lp_mode_name);
 			if (n >= 0)
-				parse_low_power_mode(fdt, n, regu, mode);
+				parse_low_power_mode(fdt, n, regu_desc, mode);
 		}
 	}
 }
@@ -608,16 +665,45 @@ static TEE_Result register_pmic_regulator(const void *fdt, struct stpmic2 *pmic,
 					  int parent_node)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
+	const struct pmic_compat_data *cdata = pmic->compat_data;
 	struct regu_dt_desc desc = { };
 	struct pmic_regu *regu = NULL;
-	size_t id = 0;
+	uint32_t index = 0;
+	uint8_t id = 0;
+	const char *desc_name = regu_name;
 
-	FMSG("Stpmic2 register %s", regu_name);
+	FMSG("Stpmic2 register %s", desc_name);
 
-	for (id = 0; id < STPMIC2_NB_REG; id++)
-		if (!strcmp(pmic_reguls[id].name, regu_name))
+	/* In case of PMIC1L PMIC2L */
+	if (!strcmp(desc_name, "buck1")) {
+		bool high = false;
+
+		res = stpmic2_is_buck1_high_voltage(pmic, &high);
+		if (res) {
+			EMSG("Failed to get %s id", desc_name);
+			return res;
+		}
+
+		if (high) {
+			FMSG("Buck1 in high voltage range");
+			desc_name = "buck1h";
+		}
+	}
+
+	/* Find the regulator platform description */
+	for (index = 0; index < cdata->desc_len; index++)
+		if (!strcmp(cdata->desc_table[index].name, desc_name))
 			break;
-	assert(id < ARRAY_SIZE(pmic_reguls));
+	assert(index < cdata->desc_len);
+
+	desc = cdata->desc_table[index];
+
+	/* Find the pmic2 regulator id */
+	res = stpmic2_regulator_get_id(desc_name, &id);
+	if (res) {
+		EMSG("Failed to get %s id", desc_name);
+		return res;
+	}
 
 	regu = calloc(1, sizeof(*regu));
 	if (!regu)
@@ -625,8 +711,6 @@ static TEE_Result register_pmic_regulator(const void *fdt, struct stpmic2 *pmic,
 
 	regu->pmic = pmic;
 	regu->id = id;
-
-	desc = pmic_reguls[id];
 	desc.priv = regu;
 
 	pmic_parse_regu_node(&desc, fdt, node);
@@ -638,7 +722,7 @@ static TEE_Result register_pmic_regulator(const void *fdt, struct stpmic2 *pmic,
 		return res;
 	}
 
-	parse_low_power_modes(fdt, node, regu);
+	parse_low_power_modes(fdt, node, &desc);
 
 	return TEE_SUCCESS;
 }
@@ -828,6 +912,7 @@ static void initialize_pmic2_i2c(const void *fdt, int node,
 static void initialize_pmic2(const void *fdt, int node,
 			     struct stpmic2 *pmic, struct i2c_handle_s *i2c)
 {
+	const struct pmic_compat_data __maybe_unused *cdata = pmic->compat_data;
 	uint8_t ver = 0;
 	uint8_t pid = 0;
 
@@ -839,17 +924,22 @@ static void initialize_pmic2(const void *fdt, int node,
 	    stpmic2_get_version(pmic, &ver))
 		panic("Failed to access PMIC");
 
+	pmic->ref_id = ((pid & PMIC_REF_ID_MASK) >> PMIC_REF_ID_SHIFT);
+
 	/* NVM version A stands for NVM_ID=1 */
-	IMSG("PMIC STPMIC25%c V%"PRIu8".%"PRIu8,
+	IMSG("PMIC STPMIC REFID:%"PRIu8".%c V%"PRIu8".%"PRIu8,
+	     pmic->ref_id,
 	     'A' + (pid & PMIC_NVM_ID_MASK) - U(1),
 	     (ver & MAJOR_VERSION_MASK) >> MAJOR_VERSION_SHIFT,
 	     ver & MINOR_VERSION_MASK);
+
+	assert(pmic->ref_id == cdata->ref_id);
 
 	stpmic2_dump_regulators(pmic);
 }
 
 static TEE_Result stm32_pmic2_probe(const void *fdt, int node,
-				    const void *compat_data __unused)
+				    const void *compat_data)
 {
 	struct stm32_i2c_dev *stm32_i2c_dev = NULL;
 	struct i2c_dev *i2c_dev = NULL;
@@ -867,6 +957,8 @@ static TEE_Result stm32_pmic2_probe(const void *fdt, int node,
 	pmic = calloc(1, sizeof(*pmic));
 	if (!pmic)
 		panic();
+
+	pmic->compat_data = compat_data;
 
 	initialize_pmic2(fdt, node, pmic, stm32_i2c_dev->handle);
 
@@ -887,8 +979,37 @@ static TEE_Result stm32_pmic2_probe(const void *fdt, int node,
 	return TEE_SUCCESS;
 }
 
+static const struct pmic_compat_data stm32_pmic25_cdata = {
+	.desc_table = pmic25_reguls,
+	.desc_len = ARRAY_SIZE(pmic25_reguls),
+	.ref_id =  PMIC_REF_ID_STPMIC25,
+};
+
+static const struct pmic_compat_data stm32_pmic1l_cdata = {
+	.desc_table = pmic1l_reguls,
+	.desc_len = ARRAY_SIZE(pmic1l_reguls),
+	.ref_id =  PMIC_REF_ID_STPMIC1L,
+};
+
+static const struct pmic_compat_data stm32_pmic2l_cdata = {
+	.desc_table = pmic2l_reguls,
+	.desc_len = ARRAY_SIZE(pmic2l_reguls),
+	.ref_id =  PMIC_REF_ID_STPMIC2L,
+};
+
 static const struct dt_device_match stm32_pmic2_match_table[] = {
-	{ .compatible = "st,stpmic2" },
+	{
+		.compatible = "st,stpmic2",
+		.compat_data = &stm32_pmic25_cdata,
+	},
+	{
+		.compatible = "st,stpmic1l",
+		.compat_data = &stm32_pmic1l_cdata,
+	},
+	{
+		.compatible = "st,stpmic2l",
+		.compat_data = &stm32_pmic2l_cdata,
+	},
 	{ }
 };
 

core/include/dt-bindings/regulator/st,stm32mp25-regulator.h
@@ -45,4 +45,11 @@
 #define VOLTD_SCMI_REGU3		29
 #define VOLTD_SCMI_REGU4		30
 
+/* GPO controls */
+#define VOLTD_SCMI_STPMIC_GPO1		31
+#define VOLTD_SCMI_STPMIC_GPO2		32
+#define VOLTD_SCMI_STPMIC_GPO3		33
+#define VOLTD_SCMI_STPMIC_GPO4		34
+#define VOLTD_SCMI_STPMIC_GPO5		35
+
 #endif /*__DT_BINDINGS_REGULATOR_STM32MP25_REGULATOR_H */

documentation/devicetree/bindings/mfd/st,stpmic2.yaml
@@ -13,8 +13,18 @@ maintainers:
 
 properties:
   compatible:
-    const: st,stpmic2
+    enum:
+      - st,stpmic1l
+      - st,stpmic2
+      - st,stpmic2l
 
+    description: |
+      The STPMIC25 uses "st,stpmic2" compatible string and can provide
+      BUCK1..7, LDO1..7, REFDDR regulators
+      The STPMIC1L uses "st,stpmic1l" compatible string and can provide
+      BUCK1, BUCK2, LDO2..5, GPO1..2 regulators
+      The STPMIC2L uses "st,stpmic2l" compatible string and can provide
+      BUCK1..4, LDO1..LDO7, GPO1..5 regulators
   reg:
     const: 0x33
 
@@ -66,22 +76,10 @@ properties:
     type: object
 
     description: |
-      Available regulators in STPMIC2 device are:
-        - buck1 for BUCK1
-        - buck2 for BUCK2
-        - buck3 for BUCK3
-        - buck4 for BUCK4
-        - buck5 for BUCK5
-        - buck6 for BUCK6
-        - buck7 for BUCK7
-        - ldo1 for LDO1
-        - ldo2 for LDO2
-        - ldo3 for LDO3
-        - ldo4 for LDO4
-        - ldo5 for LDO5
-        - ldo6 for LDO6
-        - ldo7 for LDO7
-        - ldo8 for LDO8
+      Available regulators in STPMIC2 devices are:
+        - buck1..7 for BUCK1..7
+        - ldo1..8 for LDO1..8
+        - gpo1..5 for GDO1..5
         - refddr for REFDDR
 
     properties:
@@ -89,7 +87,7 @@ properties:
         const: st,stpmic2-regulators
 
     patternProperties:
-      "^(buck[1-7]|ldo[1-8]|refddr)$":
+      "^(buck[1-7]|ldo[1-8]|gdo[1-5]|refddr)$":
         type: object
         $ref: ../regulator/regulator.yaml
 

cmadianga/optee_os

commit 9485ffbf45b3aec1831b75a2e3e8d4791ffa42a1

drivers: stm32mp2_pmic: fix the define for BUCK1 range voltage setting
Fix name of bit 7 of NVM_BUCK1_VOUT_SHR to BUCK1_VRANGE_CFG
to select the BUCK1 range voltage setting (1 for High voltage range).

Fixes: e69b46dbd78e ("drivers: stm32mp2_pmic: handle STPMIC1L and STPMIC2L")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: Id4d07506b8ec4e286b78fe05b58acf78b849a1f4
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/487495
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/drivers/stpmic2.c
@@ -610,7 +610,7 @@ TEE_Result stpmic2_is_buck1_high_voltage(struct stpmic2 *pmic, bool *high)
 
 	res = stpmic2_register_read(pmic,  NVM_BUCK1_VOUT_SHR, &val);
 
-	*high = val & BUCK1_VRAN_GE_CFG;
+	*high = val & BUCK1_VRANGE_CFG;
 
 	return res;
 }

core/include/drivers/stpmic2.h
@@ -287,7 +287,7 @@ enum {
 #define FS_OCP_LDO8		BIT(7)
 
 /* NVM_BUCK1_VOUT_SHR */
-#define BUCK1_VRAN_GE_CFG	BIT(7)
+#define BUCK1_VRANGE_CFG	BIT(7)
 
 enum stpmic2_prop_id {
 	STPMIC2_MASK_RESET = 0,