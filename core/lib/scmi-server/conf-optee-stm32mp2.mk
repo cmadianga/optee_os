gradhocJcalvo/optee-graddeck

commit 5ccf48cfd9337cc2bf7329b4e0d5126a5b8bc378

plat-stm32mp2: conf: enable SCP DVFS
Enable SCMI performance in SCP firmware for STM32MP2.

Change-Id: I319e7075abd09fbd5ea987fc09e7fb88f70bbf19
Signed-off-by: Pascal Paillet <p.paillet@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/371772
Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>

core/lib/scmi-server/conf-optee-stm32mp2.mk
@@ -1,20 +1,21 @@
 $(call force,CFG_SCPFW_MOD_CLOCK,y)
-$(call force,CFG_SCPFW_MOD_DVFS,n)
+$(call force,CFG_SCPFW_MOD_DVFS,y)
 $(call force,CFG_SCPFW_MOD_MSG_SMT,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_CLOCK,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_CONSOLE,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_MBX,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_RESET,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_VOLTD_REGULATOR,y)
-$(call force,CFG_SCPFW_MOD_PSU,n)
-$(call force,CFG_SCPFW_MOD_PSU_OPTEE_REGULATOR,n)
+$(call force,CFG_SCPFW_MOD_PSU,y)
+$(call force,CFG_SCPFW_MOD_PSU_OPTEE_REGULATOR,y)
 $(call force,CFG_SCPFW_MOD_RESET_DOMAIN,y)
 $(call force,CFG_SCPFW_MOD_SCMI,y)
 $(call force,CFG_SCPFW_MOD_SCMI_CLOCK,y)
-$(call force,CFG_SCPFW_MOD_SCMI_PERF,n)
+$(call force,CFG_SCPFW_MOD_SCMI_PERF,y)
 $(call force,CFG_SCPFW_MOD_SCMI_RESET_DOMAIN,y)
 $(call force,CFG_SCPFW_MOD_SCMI_VOLTAGE_DOMAIN,y)
 $(call force,CFG_SCPFW_MOD_VOLTAGE_DOMAIN,y)
+$(call force,CFG_SCPFW_SCMI_PERF_PROTOCOL_OPS,y)
 
 # Info level is sufficient for scp-firmware
 CFG_SCPFW_LOG_LEVEL ?= 1

cmadianga/optee_os

commit efa8b06e0845c519ec9075302a54607b741a081a

plat-stm32mp2: conf: fix PSU_OPTEE_REGULATOR configuration
Remove line for CFG_SCPFW_MOD_PSU_OPTEE_REGULATOR as it is
forced enable previously

Fixes: 5ccf48cfd933 ("plat-stm32mp2: conf: enable SCP DVFS")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: Id5d15a89f8b6206133851cb456ef6c7fa865b685
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/477821
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/lib/scmi-server/conf-optee-stm32mp2.mk
@@ -35,5 +35,4 @@ $(call force,CFG_SCPFW_NOTIFICATION,n)
 $(call force,CFG_SCPFW_FAST_CHANNEL,n)
 
 # Default configuration values for product specific modules
-CFG_SCPFW_MOD_PSU_OPTEE_REGULATOR ?= n
 CFG_SCPFW_MOD_STM32_PD ?= n